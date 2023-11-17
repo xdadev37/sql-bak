98c7f25dd78d128af7b0daca8c7142453b95a65a
PouRia SA <pouriasa.work@outlook.com>
2023-11-17 13:54:50 +0330

    updated

--- sqlcmd/db-bak-diff.sh
+++ sqlcmd/db-bak-diff.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [${SUB}-db] TO DISK = '/var/opt/mssql/data/${SUB}-db-$(date --iso)-$(date +%T).bak' WITH DIFFERENTIAL"
+/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [${SUB}-db] TO DISK = '/var/opt/mssql/diff-backups/${SUB}-db-$(date --iso)-$(date +%T).bak' WITH DIFFERENTIAL"
