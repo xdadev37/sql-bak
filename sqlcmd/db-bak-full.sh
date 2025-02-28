4beb8fdae6480b1a88eeb0021ea22c65cd8c44dd
xdadev37 <pouriasa37@gmail.com>
2025-02-28 12:29:38 +0000

    sql-bak

--- sqlcmd/db-bak-full.sh
+++ sqlcmd/db-bak-full.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [${SUB}-db-02] TO DISK = '/var/opt/mssql/full-backups/${SUB}-db-$(date --iso).bak'"
+/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [sfh-db] TO DISK = '/var/opt/mssql/full-backups/sfh-db-$(date --iso).bak'"
