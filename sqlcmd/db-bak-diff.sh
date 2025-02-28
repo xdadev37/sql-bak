6ff4e9216592711d0e5578073545c2bddf1017d7
xdadev37 <pouriasa37@gmail.com>
2025-02-28 16:45:22 +0000

    sql-bak

--- sqlcmd/db-bak-diff.sh
+++ sqlcmd/db-bak-diff.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [sfh-db-02] TO DISK = '/var/opt/mssql/diff-backups/sfh-db-$(date --iso)-$(date +%T).bak' WITH DIFFERENTIAL"
+/opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [sfh-db] TO DISK = '/var/opt/mssql/diff-backups/sfh-db-$(date --iso)-$(date +%T).bak' WITH DIFFERENTIAL"
