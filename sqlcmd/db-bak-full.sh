80879651375e103b66d9c8bab366812e1f9f8194
xdadev37 <pouriasa37@gmail.com>
2025-05-22 10:47:52 +0000

    sepidardb

--- sqlcmd/db-bak-full.sh
+++ sqlcmd/db-bak-full.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [sfh-db] TO DISK = '/var/opt/mssql/full-backups/sfh-db-$(date --iso).bak'"
+/opt/mssql-tools/bin/sqlcmd -S sepidardb -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [Sepidar01] TO DISK = '/var/opt/mssql/full-backups/Sepidar01-$(date --iso).bak'"
+/opt/mssql-tools/bin/sqlcmd -S sepidardb -U sa -P "4696048@Saman@Tabesh!" -Q "BACKUP DATABASE [Sepidar02] TO DISK = '/var/opt/mssql/full-backups/Sepidar02-$(date --iso).bak'"
