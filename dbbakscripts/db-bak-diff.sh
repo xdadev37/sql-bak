fd1a0ba43a3316630104acb1f05f5a0e9dba1149
xdadev37 <pouriasa37@gmail.com>
2026-06-24 20:00:25 +0000

    mc

--- dbbakscripts/db-bak-diff.sh
+++ dbbakscripts/db-bak-diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S "RAYDB" -U sa -P "Saman@Tabesh!" -Q "BACKUP DATABASE [RayStationResourceDB] TO URL = 's3://ms3.legit.net:9000/sepidarbak/Sepidar01-$(date +%H).bak' WITH INIT, SKIP, DIFFERENTIAL, COMPRESSION"
-/opt/mssql-tools/bin/sqlcmd -S "RAYDB" -U sa -P "Saman@Tabesh!" -Q "BACKUP DATABASE [RayStationClinicDB] TO URL = 's3://ms3.legit.net:9000/sepidarbak/Sepidar01-$(date +%H).bak' WITH INIT, SKIP, DIFFERENTIAL, COMPRESSION"
+/opt/mssql-tools/bin/sqlcmd -S ${ADDRESS} -U sa -P ${PASSWORD} -Q "BACKUP DATABASE [${DB1}] TO URL = 's3://${S3URL}/${S3BUCKET}/${DB1}-$(date +%H).bak' WITH INIT, SKIP, DIFFERENTIAL, COMPRESSION"
+/opt/mssql-tools/bin/sqlcmd -S ${ADDRESS} -U sa -P ${PASSWORD} -Q "BACKUP DATABASE [${DB2}] TO URL = 's3://${S3URL}/${S3BUCKET}/${DB2}-$(date +%H).bak' WITH INIT, SKIP, DIFFERENTIAL, COMPRESSION"
--- dbbakscripts/db-bak-even.sh
+++ dbbakscripts/db-bak-even.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S "RAYDB" -U sa -P "Saman@Tabesh!" -Q "BACKUP DATABASE [RayStationResourceDB] TO URL = 's3://ms3.legit.net:9000/sepidarbak/Sepidar01-even.bak' WITH INIT, SKIP, COMPRESSION"
-/opt/mssql-tools/bin/sqlcmd -S "RAYDB" -U sa -P "Saman@Tabesh!" -Q "BACKUP DATABASE [RayStationClinicDB] TO URL = 's3://ms3.legit.net:9000/sepidarbak/Sepidar01-even.bak' WITH INIT, SKIP, COMPRESSION"
+/opt/mssql-tools/bin/sqlcmd -S ${ADDRESS} -U sa -P ${PASSWORD} -Q "BACKUP DATABASE [${DB1}] TO URL = 's3://${S3URL}/${S3BUCKET}/${DB1}-even.bak' WITH INIT, SKIP, COMPRESSION"
+/opt/mssql-tools/bin/sqlcmd -S ${ADDRESS} -U sa -P ${PASSWORD} -Q "BACKUP DATABASE [${DB2}] TO URL = 's3://${S3URL}/${S3BUCKET}/${DB2}-even.bak' WITH INIT, SKIP, COMPRESSION"
  
